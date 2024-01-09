9cc79472e9a6245a26238b01e3bc2eb0498e5701
cbarba414 <cristinabarba846@gmail.com>
2024-01-09 08:23:57 -0800

    first commit

--- src/Picture.java
+++ src/Picture.java
@@ -42,7 +42,7 @@ public class Picture
 
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
-            roof.moveVertical(0);
+            roof.moveVertical(10);
             roof.makeVisible();
 
             sun.changeColor("yellow");
